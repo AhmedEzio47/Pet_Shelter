7e82739e0e7c38701e5127ab5fbf82a3b524255e
Ahmed <ahmednab93@gmail.com>
2021-11-29 20:50:38 +0200

    -Added uri matcher variable

--- app/src/main/java/com/devyat/petshelter/data/PetProvider.java
+++ app/src/main/java/com/devyat/petshelter/data/PetProvider.java
@@ -3,6 +3,7 @@ package com.devyat.petshelter.data;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -12,7 +13,11 @@ import androidx.annotation.Nullable;
 public class PetProvider extends ContentProvider {
     private PetDbHelper mPetDbHelper;
     private Context mContext;
-
+    public static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    static {
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS, 1);
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PET_ID, 2);
+    }
     public void PetDbHelper(Context context) {
         this.mContext = context;
     }
